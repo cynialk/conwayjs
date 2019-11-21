3c17ee093ea16cd5824325c1838111c30a331b21
Qupergo <45945560+Qupergo@users.noreply.github.com>
2019-11-21 10:48:57 +0100

    Added create_grid function
    Changed some css and added create_grid function

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//bingding chingping
-//Hello its me
+
+function create_grid(size) {
+    x_size = size[0];
+    y_size = size[1];
+    let table = document.createElement('table');
+    table.classList.add("table");
+    table.classList.add("table-bordered");
+    table.classList.add("table-dark")
+    for (let y = 0; y < y_size; y++) {
+        let tr = document.createElement('tr');
+        for (let x = 0; x < x_size; x++) {
+            let td = document.createElement('td');
+            td.classList.add("empty");
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    document.body.appendChild(table);
+}
+
+create_grid([100, 100]);
