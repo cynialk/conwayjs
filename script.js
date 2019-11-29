29f2c38bf88da03e70eadae010235cb2f762f092
cynialk <oscar02.salomonsson@gmail.com>
2019-11-29 08:33:48 +0100

    Tile & Simulation class

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //bingding chingping
 //Hello its me
+
+//
+class Tile {
+    constructor(coordinates){
+        this.position = coordinates;
+        this.state = "empty";
+    }
+
+    changeState(toState){
+        this.state = toState;
+    }
+}
+
+class Simulation {
+    constructor(board, size){
+        
+    }
+}
